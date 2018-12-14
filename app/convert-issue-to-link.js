e346c66fcab801ca9a5679d68df75735c78e7ffd
Yeongjun.Kim <yeongjun.kim@worksmobile.com>
2018-12-14 16:53:30 +0900

    refactoring

--- app/convert-issue-to-link.js
+++ app/convert-issue-to-link.js
@@ -1,26 +1,39 @@
+const JIRA_ISSUE_REGEX = /ADDRSUS-\d+/;
+
 let lastRefresh = new Date().getTime();
 
 const convert = () => {
   const current = window.location.href;
 
+  // oss issue, pr
   if (/oss\.navercorp\.com\/.*\/(issues|pull)\/\d+$/.test(current)) {
-    const title = document
-      .getElementById("partial-discussion-header")
-      .getElementsByClassName("js-issue-title")[0];
+    const $title = document.querySelector(
+      "#partial-discussion-header .js-issue-title"
+    );
+    if (!$title || $title.getAttribute("added-jira-link")) return;
 
-    if (/ADDRSUS-\d+/.test(title.innerText)) {
-      const issue = title.innerText.match(/ADDRSUS-\d+/)[0];
+    const issue = $title.innerText.match(JIRA_ISSUE_REGEX)[0];
+    if (!issue) return;
 
-      title.innerHTML = title.innerText
-        .split(/ADDRSUS-\d+/)
-        .join(
-          `<a href="http://bts1.navercorp.com/nhnbts/browse/${issue}">${issue}</a>`
-        );
-    }
+    $title.innerHTML = $title.innerText
+      .split(JIRA_ISSUE_REGEX)
+      .join(
+        `<a href="http://bts1.navercorp.com/nhnbts/browse/${issue}">${issue}</a>`
+      );
+    $title.setAttribute("added-jira-link", true);
   }
 
-  if (/oss\.navercorp\.com\/.*\/issues/.test(current)) {
-    // TODO: 이슈/PR 리스트에서도 링크 변환하기
+  // wiki 주간보고
+  if (/wiki\.navercorp\.com\/pages\/viewpage.action/.test(current)) {
+    const $content = document.getElementById("main-content");
+
+    if (!$content || $content.getAttribute("added-jira-link")) return;
+
+    // TODO: 변환
+    // http://bts1.navercorp.com/nhnbts/rest/api/latest/issue/ADDRSUS-11363?fields=status
+    // fields.status.name
+
+    $content.setAttribute("added-jira-link", true);
   }
 };
 
